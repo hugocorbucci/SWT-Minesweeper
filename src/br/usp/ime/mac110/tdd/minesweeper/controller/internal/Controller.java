42bca22b94491cf10fd48a1844a969fd2c89c5e4
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-06-21 01:24:16 -0300

    zerando para poder começar do zero amanhã. Hugo

--- src/br/usp/ime/mac110/tdd/minesweeper/controller/internal/Controller.java
+++ src/br/usp/ime/mac110/tdd/minesweeper/controller/internal/Controller.java
@@ -1,20 +1,11 @@
 package br.usp.ime.mac110.tdd.minesweeper.controller.internal;
 
-import java.util.Random;
-
 import br.usp.ime.mac110.tdd.minesweeper.controller.MineSweeperController;
 import br.usp.ime.mac110.tdd.minesweeper.model.MineSweeperBoard;
-import br.usp.ime.mac110.tdd.minesweeper.model.internal.BoardFactory;
 
 public class Controller implements MineSweeperController {
-	private BoardFactory factory;
-
-	public Controller() {
-		factory = new BoardFactory(new Random());
-	}
-	
 	public MineSweeperBoard newGame(int width, int height, int bombCount) {
-		return factory.createBoard(width, height, bombCount);
+		return null;
 	}
 
 	public void open(int line, int column) {
